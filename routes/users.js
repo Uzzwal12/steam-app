7a3f568070390390577ca812814336b6b0c1eff6
uzzwal <uzzwalagarwal@gmail.com>
2020-12-08 16:02:18 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,11 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("Invalid email or password");
+  let user = await User.find({
+    email: req.body.email,
+    username: req.body.username,
+  });
+  if (user) return res.status(400).send("User alredy exists");
 
   user = new User(req.body, [
     "name",
