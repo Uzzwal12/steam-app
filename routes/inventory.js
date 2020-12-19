e1ec766582f596a8d4122955be795cb97bf5d79b
uzzwal <uzzwalagarwal@gmail.com>
2020-12-19 18:26:10 +0530

    changes

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const { User } = require("../models/user");
-const { syncInventory } = require("../controller/inventory");
+const { getInventory } = require("../controller/inventory");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	let queryValues = req.query.id;
-	syncInventory(queryValues,function(err,result){
-		if(err){
-			console.log("error--->",err);
-		} else{
-			console.log("Data--->",result);
-			res.json(result);
-		}
-	});
+  let queryValues = req.query.id;
+  getInventory(queryValues, function (err, result) {
+    if (err) {
+      console.log("error--->", err);
+    } else {
+      console.log("Data--->", result);
+      res.json(result);
+    }
+  });
 });
 
 module.exports = router;
